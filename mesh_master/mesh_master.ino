3b3d808315aa8fe6dfc1f9d39b356e693351bc11
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2024-03-04 13:57:51 +0800

    Update mesh_master.ino

--- mesh_master/mesh_master.ino
+++ mesh_master/mesh_master.ino
@@ -23,7 +23,7 @@ void sendMessage() ; // Prototype so PlatformIO doesn't complain
 Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
 
 void sendMessage() {
-  String msg = "Hi from node1";
+  String msg = "Hi from node";
   msg += mesh.getNodeId();
   mesh.sendBroadcast( msg );
   taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
