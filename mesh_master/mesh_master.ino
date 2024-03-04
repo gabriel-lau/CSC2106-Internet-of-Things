9e59d79eda5645f4f74004df1e3b8a7d7621ebc0
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2024-03-04 17:51:04 +0800

    Wifi on master

--- mesh_master/mesh_master.ino
+++ mesh_master/mesh_master.ino
@@ -7,15 +7,17 @@
 
 #include "painlessMesh.h"
 #include <M5StickCPlus.h>
+#include <WiFi.h>
 
 #define   MESH_PREFIX     "whateverYouLike"
 #define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
 
+const char* ssid       = "opennet_2.4GHz";
+const char* password   = "98314423";
+
 Scheduler userScheduler; // to control your personal task
 painlessMesh  mesh;
-
-
 
 // User stub
 void sendMessage() ; // Prototype so PlatformIO doesn't complain
@@ -23,10 +25,10 @@ void sendMessage() ; // Prototype so PlatformIO doesn't complain
 Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
 
 void sendMessage() {
-  String msg = "Hi from node";
+  String msg = "Are you alive";
   msg += mesh.getNodeId();
   mesh.sendBroadcast( msg );
-  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+  taskSendMessage.setInterval(TASK_SECOND * 60);
   Serial.println(msg);
 }
 
@@ -37,6 +39,12 @@ void receivedCallback( uint32_t from, String &msg ) {
 
 void newConnectionCallback(uint32_t nodeId) {
     Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+    std::list<uint32_t> x = mesh.getNodeList();
+    Serial.printf("startHere: Node list: ");
+    for (int i = 0; i < x.size(); i++) {
+      Serial.printf("%u \n", x.front());
+      x.pop_front();
+    }
 }
 
 void changedConnectionCallback() {
@@ -54,7 +62,8 @@ void setup() {
   M5.Lcd.setRotation(3);
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setCursor(0, 0, 2);
-  M5.Lcd.printf("Mesh_Node", 0);
+  M5.Lcd.printf("Mesh_Master_Node", 0);
+
 
 //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
@@ -65,11 +74,21 @@ void setup() {
   mesh.onChangedConnections(&changedConnectionCallback);
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
-  userScheduler.addTask( taskSendMessage );
-  taskSendMessage.enable();
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+
+  //userScheduler.addTask( taskSendMessage );
+  //taskSendMessage.enable();
 }
+
 
 void loop() {
   // it will run the user scheduler as well
   mesh.update();
+
 }
