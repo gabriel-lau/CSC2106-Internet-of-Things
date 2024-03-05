347069a6ecfa05b8ead67cf4f55a4a89ff336131
ProneToAdjust <ryanong14@gmail.com>
2024-03-06 01:06:40 +0800

    Implement mqtt payload processing and mesh broadcast

--- mesh_master/mesh_master.ino
+++ mesh_master/mesh_master.ino
@@ -13,14 +13,10 @@
 #define   MESH_PASSWORD   "homeMesh"
 #define   MESH_PORT       5555
 
-#define   WIFI_SSID     ""
-#define   WIFI_PASSWORD ""
+#define   WIFI_SSID     "RyanLegion"
+#define   WIFI_PASSWORD "thisisthepassword"
 // Channel set to 6. Make sure to use the same channel for your mesh and for you other network (STATION_SSID)
 #define   WIFI_CHANNEL 6
-
-// prototypes
-void receivedCallback( uint32_t from, String &msg );
-void mqttCallback(char* topic, byte* payload, unsigned int length);
 
 IPAddress getlocalIP();
 
@@ -28,7 +24,13 @@ IPAddress myIP(0,0,0,0);
 
 painlessMesh  mesh;
 WiFiClient wifiClient;
-PubSubClient mqttClient("test.mosquitto.org", 1883, mqttCallback, wifiClient);
+PubSubClient mqttClient(wifiClient);
+
+String nodes[] = {"light", "fan", "aircon"};
+
+// prototypes
+void receivedCallback( uint32_t from, String &msg );
+void mqttCallback(char* topic, byte* payload, unsigned int length);
 
 void setup() {
   Serial.begin(115200);
@@ -44,13 +46,15 @@ void setup() {
   mesh.setRoot(true);
   // This node and all other nodes should ideally know the mesh contains a root, so call this on all nodes
   mesh.setContainsRoot(true);
-
-
   mesh.onReceive(&receivedCallback);
+
+  mqttClient.setServer("test.mosquitto.org", 1883);
+  mqttClient.setCallback(mqttCallback);
 }
 
 void loop() {
   mesh.update();
+  mqttClient.loop();
   
   if(myIP != getlocalIP()){
     myIP = getlocalIP();
@@ -58,7 +62,11 @@ void loop() {
 
     if (mqttClient.connect("painlessMeshClient")) {
       mqttClient.publish("iot_mesh","Ready!");
-      mqttClient.subscribe("iot_mesh");
+      
+      for (String node : nodes)
+      {
+        mqttClient.subscribe(("to/" + node).c_str());
+      }
     } 
   }
 }
@@ -68,42 +76,30 @@ void receivedCallback( uint32_t from, String &msg ) {
   mqttClient.publish("iot_mesh", msg.c_str());
 }
 
-void mqttCallback(char* topic, uint8_t* payload, unsigned int length) {
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
   char* cleanPayload = (char*)malloc(length+1);
   memcpy(cleanPayload, payload, length);
   cleanPayload[length] = '\0';
-  String msg = String(cleanPayload);
+  String payloadStr = String(cleanPayload);
   free(cleanPayload);
 
-  String targetStr = String(topic).substring(16);
+  char* cleanTopic = (char*)malloc(strlen(topic)+1);
+  strcpy(cleanTopic, topic);
+  cleanTopic[strlen(topic)] = '\0';
+  String topicStr = String(cleanTopic);
+  free(cleanTopic);
 
-  if(targetStr == "gateway")
-  {
-    if(msg == "getNodes")
-    {
-      auto nodes = mesh.getNodeList(true);
-      String str;
-      for (auto &&id : nodes)
-        str += String(id) + String(" ");
-      mqttClient.publish("iot_mesh", str.c_str());
-    }
-  }
-  else if(targetStr == "broadcast") 
-  {
-    mesh.sendBroadcast(msg);
-  }
-  else
-  {
-    uint32_t target = strtoul(targetStr.c_str(), NULL, 10);
-    if(mesh.isConnected(target))
-    {
-      mesh.sendSingle(target, msg);
-    }
-    else
-    {
-      mqttClient.publish("iot_mesh", "Client not connected!");
-    }
-  }
+  Serial.printf("Topic: %s\n", topicStr.c_str());
+  Serial.printf("Payload: %s\n", payloadStr.c_str());
+
+  // Split the topic from the slash
+  String deviceName = topicStr.substring(3);
+
+  String broadcast_message = "{device:'" + deviceName + "'" + "," + "command" + ":" + "'" + payloadStr + "'" + "}";
+
+  Serial.println("Broadcast message: " + broadcast_message);
+
+  mesh.sendBroadcast(broadcast_message);
 }
 
 IPAddress getlocalIP() {
